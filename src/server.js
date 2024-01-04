b77cfe992b57aaf3a29dfec6e0af33250cdc7f69
Vitor Belluzzo <vitorbelluzzo@hotmail.com>
2024-01-04 00:22:45 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
-const a = 5;
-const b = 5;
-console.log(a + b);
+import http from 'node:http'
+
+//CommmonJS = require
+//ESmodules => import/export
+
+//criando usuario (name, email, senha)
+
+const server = http.createServer ((request, response) => {
+  return response.end('Hello World')
+
+})
+
+server.listen(3333)
+
+//localhost:3333
+
