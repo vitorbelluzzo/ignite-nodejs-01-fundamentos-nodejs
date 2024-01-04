aa19f15cc0084a88f3978ad041a4f6c23d85b22e
Vitor Belluzzo <vitorbelluzzo@hotmail.com>
2024-01-04 19:46:37 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,12 +1,41 @@
 import http from 'node:http'
 
-//CommmonJS = require
-//ESmodules => import/export
+// - criar usuarios
+// - listagem usuarios
+// - edicao de usuarios
+// - remocao de usuarios
 
-//criando usuario (name, email, senha)
+// -http
+// - metodo http
+// - url
+
+// GET. POST, PUT, PATCH, DELETE
+
+// GET => Buscar recurso do back-end
+// POST=> Criar uma recurso no backend
+// PUT => atualizar um recurso no backend
+// PATCH => Atualizar uma info especifica de um recurso no backend
+// DELETE => Deletar um recurso do backend
+
+// EXEMPLO: GET /Users -> buscando usuarios no backend
+            // POST /Users -> criando usuarios no backend
+
 
 const server = http.createServer ((request, response) => {
-  return response.end('Hello World')
+  const { method, url } = request
+
+  if (method === 'GET' && url === '/users') {
+    // early return
+    return response.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    // early return
+    return response.end('Criação de usuário')
+  }
+
+
+  return response.end()
 
 })
 
