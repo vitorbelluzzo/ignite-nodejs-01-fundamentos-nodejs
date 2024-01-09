85d25d317e89ed6ae757221fc9a732257a3450f4
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-01-09 00:34:03 -0300

    Streams no Node.js - Corpo da requisição em JSON (Stream & Buffers) utilizando insomnia

--- src/server.js
+++ src/server.js
@@ -34,22 +34,36 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer ((request, response) => {
+const server = http.createServer (async (request, response) => {
   const { method, url } = request
 
-  console.log(request.headers);
+  const buffers =  []
+
+  // async/await
+
+  for await (const chunk of request) {
+    buffers.push(chunk) 
+  }
+
+try {
+  request.body = JSON.parse(Buffer.concat(buffers).toString())
+} catch {
+  request.body = null
+}
 
   if (method === 'GET' && url === '/users') {
-
-    return response.setHeader('Content-type', 'application/json')
+    return response
+    .setHeader('Content-type', 'application/json')
     .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const {name, email  } = request.body
+
       users.push({
         id: 1,
-        name: 'Vitor',
-        email: 'vitor@exemplo.com',
+        name,
+        email,
       })
 
     return response.writeHead(201).end()
